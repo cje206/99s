704f1b867ac8cb580472798904b220884217832a
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-03-28 12:40:44 +0900

    mainPage Img slide 및 redux 설치

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './styles/reset.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Signup from './pages/Signup';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { Provider } from 'react-redux';
+// import { composeWithDevTools } from '@redux-devtools/extension';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// const store = configureStore({ reducer: rootReducer }, composeWithDevTools);
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    {/* <Provider store ={store} > */}
+    {/* 나중에 redux 써서 store쓰면 이 안에다가 App 넣으면 됌 */}
+    {/* </Provider> */}
+    <App />
   </React.StrictMode>
 );
 
