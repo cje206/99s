a52fe192808d99d0266e802203e07a899a0b2c9d
cje206 <154851961+cje206@users.noreply.github.com>
2024-03-29 11:47:06 +0900

    Delete server directory

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 8000;
-const server = require('http').createServer(app);
-
-server.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
