13be9bf031861fc3cd874691e76384c83d070c44
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-04-17 00:35:14 +0900

    페이지 별 title 변경

--- client/src/pages/Content.tsx
+++ client/src/pages/Content.tsx
@@ -51,6 +51,10 @@ export default function ContentPage() {
     };
     getBlog();
   }, [id, postId]);
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `블로그`;
+  }, []);
 
   return (
     <div className="wrap">
